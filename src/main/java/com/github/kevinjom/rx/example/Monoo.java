e3d9f9ba5077b439aa5d5f70962a044326043641
kevinjom <gokevinvan@gmail.com>
2020-06-07 14:12:04 +0800

    Create our own Mono - Monoo, initial test case

---
+++ src/main/java/com/github/kevinjom/rx/example/Monoo.java
@@ -0,0 +1,9 @@
+package com.github.kevinjom.rx.example;
+
+import org.reactivestreams.Publisher;
+
+public class Monoo<T> {
+    public static <T> Publisher<T> just(T value) {
+        return null;
+    }
+}
---
+++ src/test/java/com/github/kevinjom/rx/example/MonooTest.java
@@ -0,0 +1,66 @@
+package com.github.kevinjom.rx.example;
+
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.*;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MonooTest {
+
+    @Test
+    void testPublisher() {
+        Publisher<Integer> publisher = Monoo.just(1);
+
+
+        List<String> invocation = new ArrayList<>();
+
+        AtomicReference<Subscription> sub = new AtomicReference<>();
+
+        publisher.subscribe(new Subscriber<>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                invocation.add("onSubscribe");
+                sub.set(s);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                invocation.add("onNext:" + integer);
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                invocation.add("onError");
+
+            }
+
+            @Override
+            public void onComplete() {
+                invocation.add("onComplete");
+            }
+        });
+
+        assertThat(invocation).containsExactly(
+                "onSubscribe"
+        );
+
+        sub.get().request(1);
+
+        assertThat(invocation).containsExactly(
+                "onSubscribe",
+                "onNext:1",
+                "onComplet"
+        );
+
+        sub.get().request(1);
+        assertThat(invocation).containsExactly(
+                "onSubscribe",
+                "onNext:1",
+                "onComplet"
+        );
+    }
+}
