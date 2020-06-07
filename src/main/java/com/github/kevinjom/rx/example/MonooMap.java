1e515249f61b9ceba0073162959b99ca2053684d
kevinjom <gokevinvan@gmail.com>
2020-06-07 16:53:36 +0800

    Implement `MonoMap.subscribe`

--- src/main/java/com/github/kevinjom/rx/example/MonooMap.java
+++ src/main/java/com/github/kevinjom/rx/example/MonooMap.java
@@ -1,6 +1,6 @@
 package com.github.kevinjom.rx.example;
 
-import org.reactivestreams.Subscriber;
+import org.reactivestreams.*;
 
 import java.util.function.Function;
 
@@ -9,13 +9,37 @@ public class MonooMap<I, O> extends Monoo<O> {
     private final Function<I, O> mapper;
 
     public MonooMap(Monoo<I> source, Function<I, O> mapper) {
-        super(); // FIXME: what do we put here?
         this.source = source;
         this.mapper = mapper;
     }
 
+    /**
+     * Monoo.just(1).map(i -> i * 2).map(i -> "hi" + i)
+     * <p>
+     * When you subscribe on this publisher, we should trigger subscription to upstream publishers
+     */
     @Override
     public void subscribe(Subscriber<? super O> s) {
+        source.subscribe(new Subscriber<I>() {
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                s.onSubscribe(subscription);
+            }
 
+            @Override
+            public void onNext(I i) {
+                s.onNext(mapper.apply(i));
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                s.onError(t);
+            }
+
+            @Override
+            public void onComplete() {
+                s.onComplete();
+            }
+        });
     }
 }
